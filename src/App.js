426800afe174b37b95fefa7bb4c83e7bcefe235b
Biswajit <biswajit.majumdar@carnationit.com>
2021-12-09 15:55:42 +0530

    basic increment decrement

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Components/Home';
+import {useSelector,useDispatch} from "react-redux"
+import {incNumber,decNumber} from "./action/action"
+
+function App() {
+  const myState = useSelector(state => state.changeTheNumber)
+  const dispatchAction = useDispatch();
+  return (
+    <div>
+      hello, world
+      <Home/>
+      <button name= "button" onClick={()=>{dispatchAction(decNumber())}}>+</button>
+      <input name = "quantity" type="text" className='quantity' value = {myState}/>
+      <button name= "button" onClick={()=>{dispatchAction(incNumber())}}>-</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/reducer/rootReducer.js
@@ -0,0 +1,7 @@
+import changeTheNumber from "./reducer";
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+    changeTheNumber
+})
+export default rootReducer;
